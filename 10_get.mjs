ef27f9b5ef3d8bcfe0a2557da3f893f41b010ce7
오지환 <ovitwisdom@gmail.com>
2025-04-23 17:22:35 +0900

    10_get.mjs / login2.html / result.html

--- 10_get.mjs
+++ 10_get.mjs
@@ -4,7 +4,7 @@ import fs from "fs"
 const app = express();
 
 app.get("/", (req, res) => {
-	fs.readFile("login.html", (err, data) => {
+	fs.readFile("login2.html", (err, data) => {
 		if(err) {
 			res.status(500);
 			return res.send("파일 읽기 오류")
@@ -18,9 +18,26 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
 	console.log("login 호출!(GET)");
 	console.log(req.query);
-	console.log("아이디", req.query.userid);
-	console.log("비밀번호", req.query.userpw);
+	const { userid, userpw } = req.query; 
+	console.log("아이디", userid);
+	console.log("비밀번호", userpw);
+
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	res.send("로그인 요청 처리 완료"); 
 })
+
+// result.html을 제공하는 라우트 추가
+app.get("/result.html", (req, res) => {
+    fs.readFile("result.html", (err, data) => {
+        if (err) {
+            res.status(500);
+            return res.send("result.html 파일 읽기 오류");
+        }
+        res.status(200).set({ "Content-Type": "text/html" });
+        res.send(data);
+    });
+});
 
 app.listen(3000, () => {
 	console.log("서버 실행 중")
