4b05c7da52d3a152b2d7210b282cea94480abd28
오지환 <ovitwisdom@gmail.com>
2025-04-24 16:59:43 +0900

    test

--- 17_session.js
+++ 17_session.js
@@ -1,45 +1,39 @@
-const express = require("express")
+const express = require("express");
 const session = require("express-session");
-
-
 const app = express();
 app.use(express.json());
-/* 
+/*
 	secret: 세션 ID 서명용 키
-	resave: 매 요청시 저장 true/false
-	saveUninitialized: 조기 빈 세션 저장 true/false
-	cookie.secure: HTTPS에서만 전송 true/false
- */
-app.use(session({
-	secret:"!@#$%^&*()",
-	resave:false,
-
-	saveUninitialized:false,
-	cookie: { secure: false },
-}))
-
+	resave: 매 요청시 세션 저장 여부
+	saveUninitialized: 초기 빈 세션 저장 여부
+	cookie.secure: HTTPS에서만 전송
+*/
+app.use(
+	session({
+		secret: "!@#$%^&*()",
+		resave: false,
+		saveUninitialized: false,
+		cookie: { secure: false },
+	})
+);
 app.post("/login", (req, res) => {
 	const { userid, password } = req.body;
-
 	// 로그인 성공시!!
 	req.session.user = { userid };
 	res.send(`로그인 성공: ${userid}`);
 });
-
-app.get('/me', (req, res) => {
-	if(req.session.user) {
+app.get("/me", (req, res) => {
+	if (req.session.user) {
 		res.json(req.session.user);
-	}else{
-		res.status(401).send('로그인이 필요합니다.')
+	} else {
+		res.status(401).send("로그인이 필요합니다.");
 	}
 });
-
-app.get('/logout', (req,res) => {
+app.get("/logout", (req, res) => {
 	req.session.destroy(() => {
-		res.send('로그아웃 되었습니다.')
+		res.send("로그아웃 되었습니다");
 	});
 });
-
 app.listen(3000, () => {
-	console.log('서버 실행 중')
-})
+	console.log("서버 실행 중");
+});
